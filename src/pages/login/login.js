e1515621a7b83f5dcb9d6d9ff50f31ff751478b0
technea <shanikhan10009@gamil.com>
2026-03-18 11:54:51 +0500

    fix build error: import toast in login.js

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import authService from '../../services/authService';
 import gsap from 'gsap';
 import '../../styles/EduFlow.css';
